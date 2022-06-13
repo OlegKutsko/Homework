1a59e4e417da0eb39d7c1d1b7fc052a1437ff51d
OlegKutsko <104584943+OlegKutsko@users.noreply.github.com>
2022-06-13 11:06:11 +0300

    Cleaner

--- пункт_3_Олег_.py
+++
@@ -1,3 +0,0 @@
-import math
-side = float(input("Введите величину стороны квадрата:"))
-print(round(float(math.pi*(side/2)**2), 2))
--- пункт_4_Олег.py
+++
@@ -1,4 +0,0 @@
-n = int(input())
-while n > 10:
-    n = sum(map(int, list(str(n))))
-print(n)
